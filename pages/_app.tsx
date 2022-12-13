f69409029986adfdf52b94dcd05b52221516a644
Austin Ftacnik <austin@ftacnik.com>
2022-12-13 14:00:16 -0800

    fix(app): type import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import AppProps from 'next/app';
+import type { AppProps } from 'next/app';
 import { WagmiConfig } from 'wagmi';
 
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
