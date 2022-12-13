7dc592835d4240eff8a202a097f43889d7b17038
nickadamson <naburner@protonmail.com>
2022-12-13 15:54:27 -0800

    chore: lint import/order

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { WagmiConfig } from 'wagmi';
-
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
 import { chains, client } from '../providers/wagmi';
-
 import RootLayout from '../components/layouts/RootLayout';
-
+import AppStateProvider from '../providers/AppStateProvider';
 import '@rainbow-me/rainbowkit/styles.css';
 import '../styles/globals.css';
-import AppStateProvider from '../providers/AppStateProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false);
