6003c04acfe1378c1544542726cdfee11aec67b7
nickadamson <naburner@protonmail.com>
2022-12-16 01:31:15 -0800

    feat: create basic card component

---
+++ components/Card.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { FC, ReactNode } from 'react';
+import cx from 'classnames';
+
+const Card: FC<{
+  onClick?: () => void;
+  className?: string;
+  children: ReactNode;
+}> = ({ onClick, className, children }) => {
+  const handleOnClick = () => onClick?.();
+
+  return (
+    <div
+      onClick={handleOnClick}
+      className={cx(
+        'flex justify-between p-2 w-full rounded-lg border border-black shadow-sm',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
