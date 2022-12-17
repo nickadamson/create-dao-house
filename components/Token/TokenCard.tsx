31c345c36074cf056baf6dc1d10e00b172dae27b
nickadamson <naburner@protonmail.com>
2022-12-16 16:50:34 -0800

    feat/wip: create token card + full components

---
+++ components/Token/TokenCard.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import { PropsWithChildren } from 'react';
+
+import { Token } from '../../data/nouns-builder-graph-types';
+import Card from '../Card';
+
+interface Props extends PropsWithChildren {
+  token: Token;
+  onClick?: () => void;
+}
+
+const TokenCard = ({ token, onClick, children }: Props) => {
+  return (
+    <Card className="hover:cursor w-80 h-[384px]" onClick={() => onClick?.()}>
+      <div className="flex flex-col w-full h-full">
+        <Image
+          // need to  decode64 sigh
+          src={token.tokenURI ?? ''}
+          alt={'Token #' + token.tokenId}
+          width={320}
+          height={320}
+        />
+        <h4 className="">#{token.tokenId}</h4>
+        <span className="">Owner: {token.owner?.id}</span>
+      </div>
+    </Card>
+  );
+};
+
+export default TokenCard;
