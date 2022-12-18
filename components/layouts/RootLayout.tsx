f9713256f879bbd00d11e2172c94267788cdbc3f
Austin Ftacnik <austin@ftacnik.com>
2022-12-17 20:59:44 -0800

    refactor(fonts): move dynamic font logic to FontWrapper component

--- components/layouts/RootLayout.tsx
+++ components/layouts/RootLayout.tsx
@@ -3,15 +3,14 @@ import NextHead from 'next/head';
 
 import Navigation from '../Navigation';
 import Footer from '../Footer';
-import { getFontWrapper } from '../../utils/styling';
+
+import FontWrapper from './Fonts/FontWrapper';
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
-  const DynamicFontWrapper = getFontWrapper();
-
   return (
     <>
       <NextHead>
@@ -20,15 +19,15 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => {
         <link rel="icon" href="/favicon.ico" /> */}
       </NextHead>
 
-      <DynamicFontWrapper>
-        <div className="flex flex-col justify-between items-center w-screen min-h-screen">
+      <FontWrapper>
+        <div className="flex flex-col items-center justify-between w-screen min-h-screen">
           <Navigation />
-          <div className="flex-grow px-5 py-5 w-full max-w-7xl xl:px-0">
+          <div className="flex-grow w-full px-5 py-5 max-w-7xl xl:px-0">
             {children}
           </div>
           <Footer />
         </div>
-      </DynamicFontWrapper>
+      </FontWrapper>
     </>
   );
 };
