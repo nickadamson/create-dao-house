21c13fcaf7a53797c2407f9a2cfdbe64d34f1bfb
nickadamson <naburner@protonmail.com>
2022-12-17 22:01:13 -0800

    feat: create label component

--- components/Proposal/ProposalCard.tsx
+++ components/Proposal/ProposalCard.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from 'react';
 
 import { Proposal } from '../../data/nouns-builder-graph-types';
 import Card from '../Card';
+import Label from '../Label';
 
 interface Props extends PropsWithChildren {
   proposal: Proposal;
@@ -10,20 +11,19 @@ interface Props extends PropsWithChildren {
 
 const ProposalCard = ({ proposal, onClick, children }: Props) => {
   return (
-    <Card className="hover:cursor" onClick={() => onClick?.()}>
+    <Card className="proposal-card hover:cursor" onClick={() => onClick?.()}>
       <div className="flex flex-col w-full">
         <div className="flex justify-between w-full">
           <h3 className="">
             {proposal.number}
             {proposal.title}
           </h3>
-          <span
-            className={`rounded-xl h-min p-0.5 ${
+          <Label
+            text={proposal.status}
+            className={`proposal-status ${
               proposal.status === 'EXECUTED' ? 'bg-green-200' : `bg-red-200`
             }`}
-          >
-            {proposal.status}
-          </span>
+          />
         </div>
         <p className="">TIMELEFT</p>
       </div>
