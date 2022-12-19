2af834b93dad4fbdf990ba2f6152e5fda8d4b469
nickadamson <naburner@protonmail.com>
2022-12-18 16:57:46 -0800

    feat: extract footer classname

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <div className="flex justify-center items-center w-full h-12 text-center bg-slate-400">
+    <div className="footer">
       <div className="max-w-7xl">foot</div>
     </div>
   );
