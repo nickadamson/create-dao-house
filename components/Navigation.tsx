4a4c19ed51e27aa2e457c44eb04089379a9ca3ae
nickadamson <naburner@protonmail.com>
2022-12-18 17:00:14 -0800

    fix/wip: move account to navbar
    still need to handle state and conditionally show

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,14 +1,17 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from 'wagmi';
 
+import { Account } from './Account';
 import Logo from './Logo';
 import NavigationLink from './NavigationLink';
 
 const Navigation = () => {
+  const { isConnected } = useAccount();
   return (
     <NavigationMenu.Root className="nav-root">
-      <NavigationMenu.List className="flex justify-center w-screen p-1 m-0 list-none max-w-7xl">
-        <div className="flex items-center justify-center w-full">
+      <NavigationMenu.List className="flex justify-center p-1 m-0 w-screen max-w-7xl list-none">
+        <div className="flex justify-center items-center w-full">
           {/* logo */}
           <NavigationMenu.Item className="flex w-1/4">
             <NavigationLink className="w-full text-lg" href="/">
@@ -16,7 +19,7 @@ const Navigation = () => {
             </NavigationLink>
           </NavigationMenu.Item>
           {/* center nav */}
-          <NavigationMenu.Sub className="flex justify-end w-1/2 ml-auto mr-0">
+          <NavigationMenu.Sub className="flex justify-end mr-0 ml-auto w-1/2">
             <NavigationMenu.Item className="w-48">
               <NavigationLink href="/vote">Vote</NavigationLink>
             </NavigationMenu.Item>
@@ -27,6 +30,7 @@ const Navigation = () => {
           {/* wallet */}
           <NavigationMenu.Item className="w-48">
             <ConnectButton />
+            {isConnected && <Account />}
           </NavigationMenu.Item>
 
           {/* <NavigationMenu.Item className='border border-red-500 border-solid'> */}
